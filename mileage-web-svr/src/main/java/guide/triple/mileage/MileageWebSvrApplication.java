86443971727ebb811c031197e28d32948e8d8788
roseoutz <roseoutz@gmail.com>
2022-07-04 02:12:15 +0900

    CQRS 및 MSA 구조 변경 중

--- mileage-web-svr/src/main/java/guide/triple/mileage/MileageWebSvrApplication.java
+++ mileage-web-svr/src/main/java/guide/triple/mileage/MileageWebSvrApplication.java
@@ -3,7 +3,9 @@ package guide.triple.mileage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class MileageWebSvrApplication {
 
